5803128f183f96ad2de5a2e1b55167dc077b3d97
subong <sasubong@startuplab.team>
2022-09-20 17:39:01 +0900

    subong

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import NotFound from '/@views/NotFound.vue';
 import Home from '/@views/Home.vue';
+import Login from '/@views/Login.vue';
+import AdminMain from '/@views/AdminMain.vue';
+import UserWorkPage from '/@views/UserWorkPage.vue';
+import UserList from '/@components/user/UserList.vue';
+import UserReg from '/@components/user/UserReg.vue';
+import UserModify from '/@components/user/UserModify.vue';
+import AdminMainView from '/@components/AdminMainView.vue';
+
+import { advancePositionWithMutation } from '@vue/compiler-core';
 
 const routes = [
   {
@@ -9,6 +18,41 @@ const routes = [
     component: Home,
   },
   { path: '/:catchAll(.*)+', name: 'NotFound', component: NotFound },
+  {
+    path: '/login',
+    name : 'Login',
+    component: Login,
+  },
+  {
+    path: '/admin',
+    name : 'AdminMain',
+    component: AdminMain,
+  },
+  {
+    path: '/admin/user/list',
+    name : 'UserList',
+    component: UserList,
+  },
+  {
+    path: '/admin/user/register',
+    name : 'UserReg',
+    component: UserReg,
+  },
+  {
+    path: '/admin/user/modify/:user_id',
+    name : 'UserModify',
+    component: UserModify,
+  },
+  {
+    path: '/admin/view',
+    name : 'AdminMainView',
+    component: AdminMainView,
+  },
+  {
+    path: '/admin/user/workpage/:user_id',
+    name : 'UserWorkPage',
+    component: UserWorkPage,
+  },
 ];
 
 const router = createRouter({
