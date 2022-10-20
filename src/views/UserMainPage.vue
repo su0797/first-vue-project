58d659072c5ace999deb7a1f14918c8d5a81b65b
suchan <limsuchan@startuplab.team>
2022-10-20 10:18:58 +0900

    데이터테이블 components로 분리작업

--- src/views/UserMainPage.vue
+++ src/views/UserMainPage.vue
@@ -38,61 +38,29 @@
     </div>
 
     <!-- 테이블 -->
-    <div class="show-nothing" v-if="dataNone">해당 데이터가 없습니다.</div>
-    <div class="show-nothing" v-if="searchedNone">해당 검색어를 찾을 수 없습니다.</div>
-    <div class="table-responsive" v-if="selectedProjectCode && selectedTaskCode && !searchedNone && !dataNone">
-      <table class="table">
-        <thead>
-          <tr>
-            <th>No.</th>
-            <th></th>
-            <th scope="col">데이터 상태</th>
-            <th scope="col">최종수정날짜</th>
-            <th scope="col" v-for="(th, i) in tableHeaderList" :key="i">
-              {{ th }}
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(data, i) in dataListValue" :key="i">
-            <td>{{ (currentPage - 1) * 100 + (i + 1) }}</td>
-            <td scope="row">
-              <router-link :to="`/user/edit/${dataList.data[i].data_id}`">
-                <button class="btn btn-secondary" type="button" id="buttonInput" @click="pushDataId(dataList.data[i].data_id)">수정</button>
-              </router-link>
-            </td>
-            <td scoped="row">
-              {{ tableTaskList[dataList.data[i].data_status] }}
-            </td>
-            <td scoped="row">
-              {{ $filters.dateFormat(dataList.data[i].update_time) }}
-            </td>
-            <td v-for="(datakey, j) in dataListKey" :key="j">
-              {{ data[datakey] }}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <div class="pagination-center" v-if="selectedProjectCode && selectedTaskCode && !searchedNone && !dataNone">
-      <vue-awesome-paginate
-        :total-items="this.totalItems"
-        :max-pages-shown="this.MaxPagesShown"
-        :current-page="this.currentPage"
-        :itemsPerPage="this.itemsPerPage"
-        :on-click="onClickHandler"
-        :show-breakpoint-buttons="false"
-        :show-ending-buttons="true"
-        firstPageContent="<<"
-        lastPageContent=">>"
-      />
-    </div>
+    <data-table
+      ref="dataTable"
+      :dataNone="this.dataNone"
+      :assignment_id="this.assignment_id"
+      :user_id="this.user_id"
+      :pjName="this.pjName"
+      :selectedProjectCode="this.selectedProjectCode"
+      :tkName="this.tkName"
+      :selectedTaskCode="this.selectedTaskCode"
+      :searchedNone="this.searchedNone"
+      :searchedData="this.searchedData"
+      :selectedSearchOption="this.selectedSearchOption"
+      @changeData="changeData"
+      @setSearchOptions="setSearchOptions"
+      @changeSearchData="changeSearchData"
+    />
   </div>
 </template>
 
 <script>
-import { msgbox } from '../service/common';
-import { getUserSearch, getUserWorkData, getUserWorkId, getUserAddForm } from '/@service/user';
+import { msgbox } from '/@service/common';
+import { getUserWorkId } from '/@service/user';
+import DataTable from '/@components/DataTable.vue';
 
 export default {
   created() {
@@ -100,31 +68,21 @@ export default {
     this.user_id = this.$cookies.get('userId');
     sessionStorage.removeItem('isAddPage');
     sessionStorage.removeItem('data_id');
-
-    const setData = new FormData();
-    setData.set('assignment_id', this.assignment_id);
-
-    getUserWorkId(setData).then((result) => {
-      this.projectList = result.data.data;
-    });
   },
-
   mounted() {
     this.pjName = sessionStorage.getItem('projectName');
     this.tkName = sessionStorage.getItem('taskName');
     this.selectedProjectCode = sessionStorage.getItem('projectCode');
     this.selectedTaskCode = sessionStorage.getItem('taskCode');
-
-    this.showTable();
+    const setData = new FormData();
+    setData.set('assignment_id', this.assignment_id);
+    getUserWorkId(setData).then((result) => {
+      this.projectList = result.data.data;
+      this.showTable();
+    });
   },
-
   data() {
     return {
-      //⬇️ paginate 변수
-      totalItems: 0,
-      itemsPerPage: 100,
-      MaxPagesShown: 5, // 페이지 숫자 버튼 값 기본값 5개
-      currentPage: 1, //  현재 활성 페이지 기본값 1
       //⬇️ user 데이터
       assignment_id: '',
       user_id: '',
@@ -135,15 +93,9 @@ export default {
       selectedProjectCode: '',
       dataNone: false,
       searchedNone: false,
-      dataList: [],
-      dataListKey: [],
-      dataListValue: [],
       searchedData: '',
       selectedSearchOption: '',
-      searchOptionList: {
-        korean: [],
-        english: [],
-      },
+      searchOptionList: {},
       taskList: [
         { tkname: '할일', tkcode: 2 },
         { tkname: '실측', tkcode: 4 },
@@ -152,11 +104,8 @@ export default {
       ],
       projectList: [],
       tableTaskList: ['삭제', '할당전', '할일', '임시저장', '실측', '조사불가', '완료'],
-      columnList: [],
-      tableHeaderList: [],
     };
   },
-
   methods: {
     showProject(e) {
       this.pjName = e.target.options[e.target.options.selectedIndex].text;
@@ -172,64 +121,21 @@ export default {
     },
     showTable() {
       if (this.pjName !== null && this.tkName !== null) {
-        const setData = new FormData();
-        this.projectCode = sessionStorage.getItem('projectCode');
-        this.projectName = sessionStorage.getItem('projectName');
-
-        setData.set('work_id', this.projectCode);
-        getUserAddForm(setData).then((result) => {
-          this.columnList = result.data;
-          this.getData();
-        });
+        this.$refs.dataTable.getTableData();
       }
     },
-    getData() {
-      const setData2 = new FormData();
-
-      setData2.set('work_id', this.selectedProjectCode);
-      setData2.set('data_status', this.selectedTaskCode);
-      setData2.set('user_id', this.user_id);
-      setData2.set('row_count', this.itemsPerPage);
-      setData2.set('page_no', this.currentPage);
-
-      getUserWorkData(setData2).then((result) => {
-        this.dataList = result.data;
-        this.totalItems = this.dataList.total_count;
-        this.tableHeaderList = [];
-
-        if (this.dataList.data.length !== 0) {
-          this.dataNone = false;
-          this.dataListKey = [];
-          this.dataListValue = [];
-          for (let i = 0; i < this.dataList.data.length; i++) {
-            const arr = JSON.parse(this.dataList.data[i].data_json);
-            this.dataListKey = Object.keys(arr);
-            this.dataListValue.push(arr);
-          }
-          for (let i = 0; i < this.columnList.data.length; i++) {
-            for (let j = 0; j < this.columnList.data.length; j++) {
-              if (this.columnList.data[j].meta_key == this.dataListKey[i]) {
-                this.tableHeaderList.push(this.columnList.data[j].meta_name);
-              }
-            }
-          }
-          this.searchOptionList.english = this.dataListKey;
-          this.searchOptionList.korean = this.tableHeaderList;
-        } else {
-          this.dataNone = true;
-        }
-      });
+    changeData(Boolean) {
+      this.dataNone = Boolean;
     },
-    onClickHandler(page) {
-      this.currentPage = page;
-      this.getData();
+    setSearchOptions(Object) {
+      this.searchOptionList = Object;
+    },
+    changeSearchData(Boolean) {
+      this.searchedNone = Boolean;
     },
     goAddForm() {
       this.$router.push('/user/add').catch(() => {});
       sessionStorage.setItem('isAddPage', true);
-    },
-    pushDataId(id) {
-      sessionStorage.setItem('data_id', id);
     },
     changeKeyword(w) {
       this.searchedData = w.target.value;
@@ -239,56 +145,22 @@ export default {
       this.searchedData = '';
     },
     searchTable() {
-      if (this.selectedSearchOption != '' && this.searchedData != '') {
-        const setSearch = new FormData();
-
-        setSearch.set('work_id', this.selectedProjectCode);
-        setSearch.set('data_status', this.selectedTaskCode);
-        setSearch.set('columnName', this.selectedSearchOption);
-        setSearch.set('keyword', this.searchedData);
-        setSearch.set('user_id', this.user_id);
-
-        getUserSearch(setSearch).then((result) => {
-          this.dataList = result.data;
-          this.totalItems = this.dataList.data.length;
-
-          for (let i = 0; i < this.dataList.data.length; i++) {
-            const arr = JSON.parse(this.dataList.data[i].data_json);
-            this.dataListKey = Object.keys(arr);
-          }
-
-          for (let i = 0; i < this.columnList.data.length; i++) {
-            for (let j = 0; j < this.columnList.data.length; j++) {
-              if (this.columnList.data[j].meta_key == this.dataListKey[i]) {
-                this.tableHeaderList.push(this.columnList.data[j].meta_name);
-              }
-            }
-          }
-          this.searchOptionList.english = this.dataListKey;
-          this.searchOptionList.korean = this.tableHeaderList;
-
-          this.dataListValue = [];
-          for (let i = 0; i < this.dataList.data.length; i++) {
-            this.dataListValue.push(JSON.parse(this.dataList.data[i].data_json));
-          }
-          if (this.dataListValue.length === 0) {
-            this.searchedNone = true;
-          }
-        });
-        this.resetSearchOption();
-      } else if (this.selectedSearchOption == '' && this.searchedData != '') {
+      if (this.selectedSearchOption !== '' && this.searchedData !== '') {
+        this.$refs.dataTable.getSearchTable();
+      } else if (this.selectedSearchOption === '' && this.searchedData !== '') {
         msgbox('검색할 칼럼을 선택해주세요.');
         this.$refs.searchSelect.focus();
-      } else if (this.selectedSearchOption != '' && this.searchedData == '') {
+      } else if (this.selectedSearchOption !== '' && this.searchedData === '') {
         msgbox('검색할 키워드를 입력해주세요.');
         this.$refs.searchInput.focus();
       } else {
         this.searchedNone = false;
         this.resetSearchOption();
-        this.showTable();
+        this.$refs.dataTable.getTableData();
       }
     },
   },
+  components: { DataTable },
 };
 </script>
 
@@ -309,25 +181,5 @@ h3 {
 }
 .btn-add-data:hover {
   background-color: #c15a33;
-}
-.pagination-center {
-  display: flex;
-  width: 100%;
-}
-.table-responsive tr th:first-child,
-.table-responsive tr td:first-child {
-  position: sticky;
-  left: 0;
-  z-index: 99;
-  min-width: 37.31px;
-}
-.table-responsive tr td:nth-child(-n + 2) {
-  background: #fff;
-}
-.table-responsive tr th:nth-child(2),
-.table-responsive tr td:nth-child(2) {
-  position: sticky;
-  left: 37.31px;
-  z-index: 99;
 }
 </style>
