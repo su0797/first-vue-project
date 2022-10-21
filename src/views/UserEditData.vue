940bc095af8dd3652763439e8aa72b1102f64fc9
suchan <limsuchan@startuplab.team>
2022-10-21 13:09:17 +0900

    selectBox 정수반환

--- src/views/UserEditData.vue
+++ src/views/UserEditData.vue
@@ -11,7 +11,7 @@
         <UserInput :label="column.meta_name" :inputValue="(inputValueList[column.meta_key] = inputValueList[column.meta_key])" @inputFromChild="inputValueList[column.meta_key] = $event.target.value" v-if="column.meta_type === '1'" />
         <UserSelectBox
           :label="column.meta_name"
-          :selectValue="(inputValueList[column.meta_key] = inputValueList[column.meta_key])"
+          :selectValue="(inputValueList[column.meta_key] = Number(inputValueList[column.meta_key]))"
           @selectFromChild="inputValueList[column.meta_key] = Number($event.target.value)"
           v-else-if="column.meta_type === '2'"
         />
